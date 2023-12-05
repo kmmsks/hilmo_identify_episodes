d72801acc885832aa38077ebcbf417a525486f69
Gutvilig <maigutvi@ad.helsinki.fi>
2023-12-05 11:46:12 +0200

    0_preparation: typos

--- R/0_preparation.R
+++ R/0_preparation.R
@@ -13,27 +13,27 @@
 #
 # 0a_preparation_funs.R
 #   - Read functions: 
-#       In case you cannot rename the raw files or organize the row data sub-folders, 
+#       In case you cannot rename the raw files or organize the raw data sub-folders, 
 #       reading the data might be a bit laborious. These read functions represent a real-world example.
 #   - Preparation functions:
-#       + process_dgs_1996_2017 and process_dgs_2018_: format of diagnoses data 
-#         are in slightly different format
-#       + process_before_1996: years 1975-1995 are divided in several files, 
+#       + process_dgs_1996_2017 and process_dgs_2018_: format of diagnosis data 
+#         is in slightly different formats
+#       + process_before_1996: years 1975-1995 are divided into several files, 
 #         which need no be handled separately, but this function contains the 
-#         parts that can be pre-processed in similar way (after varying pre-pre-processing)
+#         parts that can be pre-processed in a similar way (after varying pre-pre-processing)
 #       + process_avohilmo: primary care data. Harmonize column names, exclude non-valid rows. 
 #   - Convert ICD-8-9-10 diagnoses:
 #         Conversion of selected ICD-8-9 codes into equivalent ICD-10.
 #   - Outpatient report: statistics about secondary care outpatient appointments
 #         within inpatient episodes.
 
-# Next, the pre-processing is in the following scipts.
-# Each dataset will be red and processed. The prcessed data will be saved into
+# Next, the pre-processing is in the following scripts.
+# Each dataset will be read and processed. The processed data will be saved into
 # "longitudinal" chunks, meaning that each individual's all data will be saved
-# into same file, using append cuntionality of fwirte function
+# into the same file, using the append functionality of the fwrite function.
 
 # 0b_preparation_before_1996.R:
-#   - These data contain only inpatient care and less variables then data since 1996.
+#   - These data contain only inpatient care and less variables than data since 1996.
 #     Hence, this is much more simple than pre-processing newer data. 
 #     Be aware of varying date formats and erroneous values, however. 
 #
@@ -43,15 +43,15 @@
 #       Secondary care inpatient and outpatient data
 #     + 0c_preparation_source_RPHC.R
 #       Primary care data
-#   - Finally, longitudinal chunks will be seved by appending.
+#   - Finally, longitudinal chunks will be saved by appending.
 
 source(here("scripts", "R", "0a_preparation_settings_dgs.R"), encoding = "UTF-8")
 
 source(here("scripts", 'R', "0a_preparation_funs.R"), encoding = 'UTF-8')
 
 
-# Create directory tree --------------------------------------------------------
-# See readme
+# Create the directory tree --------------------------------------------------------
+# See README
 
 settings$data_folder_name <- 'data_main'
 dirs <- c( dirs0, create_dirs_preprocess(data_folder_name = settings$data_folder_name))
@@ -61,9 +61,8 @@ dirs <- c( dirs0, create_dirs_preprocess(data_folder_name = settings$data_folder
 
 # initially data are in sub-parts based on years.
 # After pre-processing, each individual's all data will be put in a single file, here called longitudinal format.
-# Individuals are grouped based on the first character in ID-variable (here called `shnro`).
-# In the next step, each group will be processed separately for performance reasons.
-
+# Individuals are grouped based on the first character of the ID-variable (here called `shnro`).
+# In the next step, each group will be processed separately for computing performance reasons.
 
 ## Old registers, before 1996 --------------------------------------------------
 
